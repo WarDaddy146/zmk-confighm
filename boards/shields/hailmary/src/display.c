52388fd14999ae4ca07b5c7b4dfe2f6ecd0984a7
Ali <ali@example.com>
2026-06-05 14:29:37 +0530

    switch pixel to lv_canvas approach

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -5,13 +5,11 @@ lv_obj_t *zmk_display_status_screen(void)
     lv_obj_t *screen = lv_obj_create(NULL);
     lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
 
-    lv_obj_t *pixel = lv_obj_create(screen);
-    lv_obj_set_size(pixel, 1, 1);
-    lv_obj_set_pos(pixel, 0, 0);
-    lv_obj_set_style_bg_color(pixel, lv_color_black(), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(pixel, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_set_style_border_width(pixel, 0, LV_PART_MAIN);
-    lv_obj_set_style_pad_all(pixel, 0, LV_PART_MAIN);
+    lv_obj_t *canvas = lv_canvas_create(screen);
+    static lv_color_t cbuf[1];
+    lv_canvas_set_buffer(canvas, cbuf, 1, 1, LV_IMG_CF_TRUE_COLOR);
+    lv_obj_set_pos(canvas, 0, 0);
+    lv_canvas_set_px(canvas, 0, 0, lv_color_black());
 
     return screen;
 }
