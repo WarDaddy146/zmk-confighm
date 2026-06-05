8eaa69c7761f80893eea8185927301e0e55e12d0
Ali <ali@example.com>
2026-06-05 14:02:50 +0530

    Use lv_color_t buffer and correct lv_canvas_set_px signature

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -1,13 +1,13 @@
 #include <lvgl.h>
 
-static uint8_t canvas_buf[16384];
+static lv_color_t cbuf[LV_CANVAS_BUF_SIZE_TRUE_COLOR(128, 32)];
 
 lv_obj_t *zmk_display_status_screen(void)
 {
     lv_obj_t *screen = lv_obj_create(NULL);
 
     lv_obj_t *canvas = lv_canvas_create(screen);
-    lv_canvas_set_buffer(canvas, canvas_buf, 128, 32, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(canvas, cbuf, 128, 32, LV_IMG_CF_TRUE_COLOR);
     lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
     lv_canvas_set_px(canvas, 0, 0, lv_color_black(), LV_OPA_COVER);
 
