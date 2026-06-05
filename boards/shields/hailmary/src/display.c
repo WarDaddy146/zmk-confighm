774e3fe6fc7f583bbdd30eabfbb14f4041177f10
Ali <ali@example.com>
2026-06-05 13:58:43 +0530

    Use hardcoded buffer size to avoid macro issues

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -1,16 +1,13 @@
 #include <lvgl.h>
 
-#define DISPLAY_WIDTH 128
-#define DISPLAY_HEIGHT 32
-
-static uint8_t canvas_buf[LV_CANVAS_BUF_SIZE_TRUE_COLOR(DISPLAY_WIDTH, DISPLAY_HEIGHT)];
+static uint8_t canvas_buf[16384];
 
 lv_obj_t *zmk_display_status_screen(void)
 {
     lv_obj_t *screen = lv_obj_create(NULL);
 
     lv_obj_t *canvas = lv_canvas_create(screen);
-    lv_canvas_set_buffer(canvas, canvas_buf, DISPLAY_WIDTH, DISPLAY_HEIGHT, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(canvas, canvas_buf, 128, 32, LV_IMG_CF_TRUE_COLOR);
     lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
     lv_canvas_set_px(canvas, 0, 0, lv_color_black(), LV_OPA_COVER);
 
