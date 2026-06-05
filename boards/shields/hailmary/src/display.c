c67714c1f87cd7707a7255fd5d58225e0b075638
Ali <ali@example.com>
2026-06-05 16:09:55 +0530

    Adding Memset and direct Pixel control

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -7,9 +7,11 @@ lv_obj_t *zmk_display_status_screen(void)
 
     lv_obj_t *canvas = lv_canvas_create(screen);
     static uint8_t cbuf[128 * 32];
+
+    memset(cbuf, 0x00, sizeof(cbuf));
     lv_canvas_set_buffer(canvas, cbuf, 128, 32, LV_COLOR_FORMAT_L8);
     lv_obj_set_pos(canvas, 0, 0);
-    lv_canvas_set_px(canvas, 0, 0, lv_color_black(), LV_OPA_COVER);
+    cbuf[0 * 128 + 0]   = 0xFF;
 
     return screen;
 }
