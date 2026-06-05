088c6a2712de355c776d5dbe585dc9f6eaffdddf
Ali <ali@example.com>
2026-06-05 14:34:24 +0530

    fix canvas: enable LV_USE_CANVAS, use LVGL v9 API

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -6,8 +6,8 @@ lv_obj_t *zmk_display_status_screen(void)
     lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
 
     lv_obj_t *canvas = lv_canvas_create(screen);
-    static lv_color_t cbuf[1];
-    lv_canvas_set_buffer(canvas, cbuf, 1, 1, LV_IMG_CF_TRUE_COLOR);
+    static uint8_t cbuf[16];
+    lv_canvas_set_buffer(canvas, cbuf, 1, 1, LV_COLOR_FORMAT_L8);
     lv_obj_set_pos(canvas, 0, 0);
     lv_canvas_set_px(canvas, 0, 0, lv_color_black());
 
