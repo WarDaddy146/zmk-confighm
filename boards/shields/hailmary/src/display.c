3274224a706224bd7d6897d9a4ded58303aa0282
Ali <ali@example.com>
2026-06-05 13:54:26 +0530

    Use LVGL Canvas instead of lv_obj for pixel drawing

--- boards/shields/hailmary/src/display.c
+++ boards/shields/hailmary/src/display.c
@@ -1,17 +1,18 @@
 #include <lvgl.h>
 
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 32
+
+static uint8_t canvas_buf[LV_CANVAS_BUF_SIZE_TRUE_COLOR(DISPLAY_WIDTH, DISPLAY_HEIGHT)];
+
 lv_obj_t *zmk_display_status_screen(void)
 {
     lv_obj_t *screen = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
 
-    lv_obj_t *pixel = lv_obj_create(screen);
-    lv_obj_set_size(pixel, 1, 1);
-    lv_obj_set_pos(pixel, 0, 0);
-    lv_obj_set_style_bg_color(pixel, lv_color_black(), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(pixel, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_set_style_border_width(pixel, 0, LV_PART_MAIN);
-    lv_obj_set_style_pad_all(pixel, 0, LV_PART_MAIN);
+    lv_obj_t *canvas = lv_canvas_create(screen);
+    lv_canvas_set_buffer(canvas, canvas_buf, DISPLAY_WIDTH, DISPLAY_HEIGHT, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
+    lv_canvas_set_px(canvas, 0, 0, lv_color_black(), LV_OPA_COVER);
 
     return screen;
 }
